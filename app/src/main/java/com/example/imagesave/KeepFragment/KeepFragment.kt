32b3af0cbd081e95c999c44187ec4967b38a4df8
이우진 <dldnwls0115@naver.com>
2024-01-31 16:49:31 +0900

    Design : 보관함 불러오기 패턴변경
    보관함 데이터 저장하고 불러오기 MVVM패턴적용

--- app/src/main/java/com/example/imagesave/KeepFragment/KeepFragment.kt
+++ app/src/main/java/com/example/imagesave/KeepFragment/KeepFragment.kt
@@ -1,17 +1,12 @@
 package com.example.imagesave.KeepFragment
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.imagesave.ImageSearchFragment.SearchViewModel
-import com.example.imagesave.R
-import com.example.imagesave.data.SearchDocument
 import com.example.imagesave.data.SelectedItem
 import com.example.imagesave.databinding.FragmentKeepBinding
 import com.google.gson.Gson
@@ -35,19 +30,24 @@ class KeepFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentKeepBinding.inflate(inflater, container, false)
+        initViewModel()
         loadMyLikeList()
-        keepAdapter = KeepAdapter(SelectedItem.myLikeList)
-        binding.keepRecyclerView.adapter = keepAdapter
-        binding.keepRecyclerView.layoutManager = GridLayoutManager(context, 2)
 
         return binding.root
+    }
+    private fun initViewModel() =with(viewModel) {
+        loadListData.observe(viewLifecycleOwner){
+            SelectedItem.myLikeList = it.toMutableList()
+            keepAdapter = KeepAdapter(SelectedItem.myLikeList)
+            binding.keepRecyclerView.adapter = keepAdapter
+            binding.keepRecyclerView.layoutManager = GridLayoutManager(context, 2)
+        }
     }
 
     /**
      * 실시간으로 업데이트하기위해 onResume상태일때 어뎁터 초기화.
      */
     override fun onResume() {
-        initViewModel()
         keepAdapter = KeepAdapter(SelectedItem.myLikeList)
         binding.keepRecyclerView.adapter = keepAdapter
         binding.keepRecyclerView.layoutManager = GridLayoutManager(context, 2)
@@ -68,30 +68,28 @@ class KeepFragment : Fragment() {
         super.onResume()
     }
 
-    private fun initViewModel() {
-
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
     private fun saveMyLikeList() {
-        val sharedPreferences = requireContext().getSharedPreferences("MyPreferences", 0)
-        val editor = sharedPreferences.edit()
-        // 데이터를 문자열로 변환하여 저장
-        val jsonMyLikeList = Gson().toJson(SelectedItem.myLikeList)
-        editor.putString("myLikeList", jsonMyLikeList)
-        editor.apply()
+        viewModel.saveMyLikeList(requireContext(), SelectedItem.myLikeList)
+//        val sharedPreferences = requireContext().getSharedPreferences("MyPreferences", 0)
+//        val editor = sharedPreferences.edit()
+//        // 데이터를 문자열로 변환하여 저장
+//        val jsonMyLikeList = Gson().toJson(SelectedItem.myLikeList)
+//        editor.putString("myLikeList", jsonMyLikeList)
+//        editor.apply()
     }
 
     private fun loadMyLikeList() {
-        val sharedPreferences = requireContext().getSharedPreferences("MyPreferences", 0)
-        val jsonMyLikeList = sharedPreferences.getString("myLikeList", null)
-        // 저장된 데이터가 있으면 해당 데이터를 객체로 변환하여 대입
-        jsonMyLikeList?.let {
-            val type = object : TypeToken<List<SelectedItem>>() {}.type
-            SelectedItem.myLikeList = Gson().fromJson(it, type)
-        }
+        viewModel.loadMyLikeList(requireContext())
+//        val sharedPreferences = requireContext().getSharedPreferences("MyPreferences", 0)
+//        val jsonMyLikeList = sharedPreferences.getString("myLikeList", null)
+//        // 저장된 데이터가 있으면 해당 데이터를 객체로 변환하여 대입
+//        jsonMyLikeList?.let {
+//            val type = object : TypeToken<List<SelectedItem>>() {}.type
+//            SelectedItem.myLikeList = Gson().fromJson(it, type)
+//        }
     }
 
     companion object {
--- app/src/main/java/com/example/imagesave/KeepFragment/KeepViewModel.kt
+++ app/src/main/java/com/example/imagesave/KeepFragment/KeepViewModel.kt
@@ -1,6 +1,32 @@
 package com.example.imagesave.KeepFragment
 
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.imagesave.data.SelectedItem
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class KeepViewModel : ViewModel() {
+    private val _loadListData: MutableLiveData<List<SelectedItem>> = MutableLiveData()
+    val loadListData: LiveData<List<SelectedItem>> get() = _loadListData
+
+    fun saveMyLikeList(context: Context, myLikeList: List<SelectedItem>) {
+        val sharedPreferences = context.getSharedPreferences("MyPreferences", 0)
+        val editor = sharedPreferences.edit()
+        // 데이터를 문자열로 변환하여 저장
+        val jsonMyLikeList = Gson().toJson(myLikeList)
+        editor.putString("myLikeList", jsonMyLikeList)
+        editor.apply()
+    }
+    fun loadMyLikeList(context: Context) {
+        val sharedPreferences = context.getSharedPreferences("MyPreferences", 0)
+        val jsonMyLikeList = sharedPreferences.getString("myLikeList", null)
+        // 저장된 데이터가 있으면 해당 데이터를 객체로 변환하여 대입
+        jsonMyLikeList?.let {
+            val type = object : TypeToken<List<SelectedItem>>() {}.type
+            _loadListData.value = Gson().fromJson(it, type)
+        }
+    }
 }
