758ebe29568835879261373ce37fff990da624d0
이우진 <dldnwls0115@naver.com>
2024-02-03 21:58:33 +0900

    Refactor : api하드코딩

--- app/src/main/java/com/example/imagesave/Contract.kt
+++ app/src/main/java/com/example/imagesave/Contract.kt
@@ -2,7 +2,8 @@ package com.example.imagesave
 
 class Contract {
     companion object{
-        const val API_KEY = BuildConfig.kakao_api_key
+//        const val API_KEY = BuildConfig.kakao_api_key
+        const val API_KEY = "826bbdb76de957ac56567d3605915a90"
         const val REQUEST_ADDRESS = "search/image"
         const val SERVICE_URL = "https://dapi.kakao.com/v2/"
         const val REQUEST_ADDRESS_VIDEO = "search/vclip"
